1a7b8a95bb4c4bcf9c6712aaaeef67f5eb397d26
psgani <131879872+psgani@users.noreply.github.com>
2025-02-13 23:50:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,14 @@ var express = require('express');
 var cors = require('cors')
 var app = express()
  
-app.use(cors(
-    {
-     origin: ["https://cc-test-p3mt.vercel.app"],
-     methods:["POST", "GET"],
-     credentails: true
-    }
-   ))
+// app.use(cors(
+//     {
+//      origin: ["https://cc-test-p3mt.vercel.app"],
+//      methods:["POST", "GET"],
+//      credentails: true
+//     }
+//    ))
+app.use(cors({ origin: "https://cc-test-p3mt.vercel.app" }));
 
 app.use(express.json())
 
